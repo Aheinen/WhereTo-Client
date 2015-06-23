c440d31a3e644d599b985f767d5c38ddb30f2894
Alex Heinen & Fernanda M M Getschko <pair+aheinen.fmgetschko@devbootcamp.com>
2015-06-23 14:01:57 -0700

    add comment to mobile.js

--- js/mobile.js
+++ js/mobile.js
@@ -2,4 +2,5 @@ $(document).bind("mobileinit", function(){
   $.mobile.allowCrossDomainPages = true;
   $.support.cors                 = true;
   $.mobile.pushStateEnabled      = false;
+  // Need this for mobile cors
 });
