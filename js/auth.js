ea5c5694cacbd3f4e6389bf3756dd71d92361870
Alex Heinen & Fernanda M M Getschko <pair+aheinen.fmgetschko@devbootcamp.com>
2015-06-23 15:10:51 -0700

    add callback function to inappbrowser on auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,7 @@
 $(document).on('pagecreate',function(event) {
+
+  document.addEventListener("deviceready", onDeviceReady, false);
+
   var ref = new Firebase("https://wheretodbc.firebaseIO.com");
 
   $('#login').on('click', function(e){
@@ -69,6 +72,14 @@ var createUser = function(obj){
     console.log("fail");
   })
 }
+
+function onDeviceReady() {
+  var ref = window.open('http://apache.org', '_blank', 'location=yes');
+  ref.addEventListener('loadstart', function(event) { alert('start: ' + event.url); });
+  ref.addEventListener('loadstop', function(event) { alert('stop: ' + event.url); });
+  ref.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
+  ref.addEventListener('exit', function(event) { alert(event.type); });
+ }
 
 
 
