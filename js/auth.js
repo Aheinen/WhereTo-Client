692e480733cce71bc4b626be0366c417aa3761e6
Alex Heinen & Fernanda M M Getschko <pair+aheinen.fmgetschko@devbootcamp.com>
2015-06-24 09:23:34 -0700

    cleanup unused code

--- js/auth.js
+++ js/auth.js
@@ -1,13 +1,10 @@
 $(document).on('pagecreate',function(event) {
 
-  // setInApp();
-
   var ref = new Firebase("https://wheretodbc.firebaseIO.com");
 
   $('#login').on('click', function(e){
    e.preventDefault();
    fbAuth().then(function(authData){
-      // alert('in the THEN for the promise');
       var firstName = authData.facebook.cachedUserProfile.first_name;
       var lastName = authData.facebook.cachedUserProfile.last_name;
       var email = authData.facebook.email;
@@ -24,7 +21,6 @@ $(document).on('pagecreate',function(event) {
       createUser(userInfo);
 
    }, { scope: "email" });
-   // onDeviceReady();
  });
 
 
@@ -32,10 +28,8 @@ var fbAuth = function(){
   var promise = new Promise(function(resolve, reject){
     ref.authWithOAuthPopup("facebook", function(error, authData) {
       if (error) {
-        // alert("login failed!");
         reject(error);
       } else {
-        // document.addEventListener("deviceready", onDeviceReady, false);
         resolve(authData);
       };
     });
@@ -44,14 +38,12 @@ var fbAuth = function(){
 };
 });
 var createUser = function(obj){
-  // alert('creating user')
   $.ajax({
     url: 'https://whereto-server.herokuapp.com/users',
     method:'POST',
     data: obj
   })
   .done(function(response){
-    // alert('in the AJAX done');
     var user_id = response.user.id.toString();
     $('#container').addClass(user_id);
     $('#container').removeClass("landing_page");
@@ -60,9 +52,7 @@ var createUser = function(obj){
       $('#footer').removeAttr('style');
 
       template = Handlebars.compile($("#single-event-template").html());
-      // var desc = "<p>Description: " + response.event.description + "</p>";
       $("#container").html(template(response));
-      // $('#container a').append(desc);
     }
     else {
       template = Handlebars.compile($("#preferences").html());
@@ -70,55 +60,10 @@ var createUser = function(obj){
       $("#container > ul").listview().listview("refresh");
     }
   console.log(window.location.href);
-  // alert(window.location.href);
   })
   .fail(function(){
     console.log("fail");
   })
 }
 
-//onLoad functions
-var onLoad = function() {
-  // alert('load')
-  document.addEventListener("deviceready", onDeviceReady, false);
-};
-
-var onDeviceReady = function() {
-  // alert('ready')
-  bindPause();
-  bindResume();
-};
-
-var bindPause = function() {
-  document.addEventListener('pause', onPause, false);
-};
-
-var onPause = function(){
-  // alert('pause')
-  Firebase.goOffline();
-};
-
-var bindResume = function(){
-  document.addEventListener('resume', onResume, false);
-};
-
-var onResume = function(){
-  clearMarkers();
-  Firebase.goOnline();
-  loadSpaces();
-  // alert("resume works")
-};
-
-var setInApp = function(){
-  var ref = cordova.InAppBrowser.open('http://192.168.1.82:8000/', '_blank', 'location=yes');
-  ref.addEventListener('loadstart', function(event) { alert(event.url); });
-};
-
-function onDeviceReady() {
-  var ref = window.open('https://auth.firebase.com/blank/page.html', '_blank', 'location=yes');
-// close InAppBrowser after 5 seconds
-  setTimeout(function() {
-      ref.close();
-  }, 500);
- }
 
