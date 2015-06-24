d7946dda84cad175ff8738652ee4f053f4553caa
Tonytino <tony91139@Gmail.com>
2015-06-23 17:27:07 -0700

    home button in menu funtions now

---
+++ js/preferences.js
@@ -0,0 +1,38 @@
+$(document).ready(function(event) {
+
+    $('#mypanel').off('click').on('click', '#preferences', function(event) {
+        event.preventDefault();
+
+        // hide header/footer
+        $('#footer').css('display', 'none')
+
+        var user_id = $('#container').attr('class');
+        var url = 'https://whereto-server.herokuapp.com/users/' + user_id + "/edit";
+
+        $.getJSON(url, function(json) {
+            console.log(json)
+
+            template = Handlebars.compile($("#preferences").html());
+            $("#container").html(template(json));
+            $("#container > ul").listview().listview("refresh");
+        })
+    }) // end update preferences
+
+    .on('click', '#home', function(event) {
+        var user_id = $('#container').attr('class');
+
+        var url = "https://whereto-server.herokuapp.com/users/" + user_id + "/events"
+
+        template = Handlebars.compile($("#single-event-template").html())
+
+        $.getJSON(url, function(json) {
+            var desc = "<p>Description: " + json.event.description + "</p>";
+            $("#container").html(template(json))
+            $('#container').find('.back').append(desc)
+            $('#footer a').removeClass('ui-btn-active');
+            $('#footer').removeAttr('style');
+        }) // end getJSON
+
+    }) // end home link
+
+}); // end document ready
