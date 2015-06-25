b6a13f27bb085a5489378920b3d24d0f02a5fb05
Alex Heinen <alexheinen99@gmail.com>
2015-06-25 12:04:16 -0700

    bring all js libraries in house

--- js/preferences.js
+++ js/preferences.js
@@ -35,4 +35,43 @@ $(document).ready(function(event) {
 
     }) // end home link
 
+    // #.off used to prevent double firing of events
+    // http://www.gajotres.net/prevent-jquery-multiple-event-triggering/ See Solution 2
+    $('#container').off('click', '#submit_preferences').on('click', '#submit_preferences', function(event) {
+        event.preventDefault();
+
+        var interests = []
+        var items = $('#container').find('li > a')
+
+        $.each(items, function( index, value ) {
+
+            if ($(this).hasClass("ui-btn-active")) {
+
+                interests.push(index+1);
+
+            } // end if loop
+        }); // end each loop
+
+        var user_id = $('#container').attr('class')
+
+        var updateInterests = $.post(
+            "https://whereto-server.herokuapp.com/users/"+user_id+"/interests",
+            {interests: interests}
+        );
+
+        updateInterests.done(function(response){
+            $('#header').removeAttr('style');
+            $('#footer').removeAttr('style');
+            template = Handlebars.compile($("#single-event-template").html());
+            // var desc = "<p>Description: " + response.event.description + "</p>";
+            $("#container").html(template(response));
+            // $('#container a').append(desc);
+        })
+
+        updateInterests.fail(function(){
+            alert("aw man");
+        })
+
+    }) // end submit preferences
+
 }); // end document ready
