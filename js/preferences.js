a08e971b104a7418f63996f40e202aa96bd6027f
Alex Heinen <alexheinen99@gmail.com>
2015-06-25 19:29:33 -0700

    GET SHERIF'D

--- js/preferences.js
+++ js/preferences.js
@@ -1,77 +1,81 @@
 $(document).ready(function(event) {
 
-    $('#mypanel').off('click').on('click', '#preferences', function(event) {
-        event.preventDefault();
+  $('#mypanel').off('click').on('click', '#preferences', function(event) {
+    event.preventDefault();
+    // hide header/footer
+    $('#footer').css('display', 'none')
 
-        // hide header/footer
-        $('#footer').css('display', 'none')
+    var user_id = $('#container').attr('class');
+    var url = baseURL + '/users/' + user_id + "/edit";
 
-        var user_id = $('#container').attr('class');
-        var url = baseURL + '/users/' + user_id + "/edit";
+    $.getJSON(url, function(json) {
+      console.log(json)
 
-        $.getJSON(url, function(json) {
-            console.log(json)
+      template = Handlebars.compile($("#preferences").html());
+      $("#container").html(template(json));
+      $("#container > ul").listview().listview("refresh");
+    })
+  }) // end update preferences
 
-            template = Handlebars.compile($("#preferences").html());
-            $("#container").html(template(json));
-            $("#container > ul").listview().listview("refresh");
-        })
-    }) // end update preferences
+  .on('click', '#home', function(event) {
+    var user_id = $('#container').attr('class');
 
-    .on('click', '#home', function(event) {
-        var user_id = $('#container').attr('class');
+    var url = baseURL + "/users/" + user_id + "/events"
 
-        var url = baseURL + "/users/" + user_id + "/events"
+    template = Handlebars.compile($("#single-event-template").html())
 
-        template = Handlebars.compile($("#single-event-template").html())
+    $.getJSON(url, function(json) {
+      var desc = "<p>Description: " + json.event.description + "</p>";
+      $("#container").html(template(json))
+      $('#container').find('.back').append(desc)
+      $('#footer a').removeClass('ui-btn-active');
+      $('#footer').removeAttr('style');
+    }) // end getJSON
+  }) // end home link
 
-        $.getJSON(url, function(json) {
-            var desc = "<p>Description: " + json.event.description + "</p>";
-            $("#container").html(template(json))
-            $('#container').find('.back').append(desc)
-            $('#footer a').removeClass('ui-btn-active');
-            $('#footer').removeAttr('style');
-        }) // end getJSON
+  // #.off used to prevent double firing of events
+  // http://www.gajotres.net/prevent-jquery-multiple-event-triggering/ See Solution 2
+  $('#container').off('click', '#submit_preferences').on('click', '#submit_preferences', function(event) {
+    event.preventDefault();
 
-    }) // end home link
+    var interests = []
+    var items = $('#container').find('li > a')
 
-    // #.off used to prevent double firing of events
-    // http://www.gajotres.net/prevent-jquery-multiple-event-triggering/ See Solution 2
-    $('#container').off('click', '#submit_preferences').on('click', '#submit_preferences', function(event) {
-        event.preventDefault();
+    $.each(items, function( index, value ) {
 
-        var interests = []
-        var items = $('#container').find('li > a')
+      if ($(this).hasClass("ui-btn-active")) {
 
-        $.each(items, function( index, value ) {
+        interests.push(index+1);
 
-            if ($(this).hasClass("ui-btn-active")) {
+          } // end if loop
+      }); // end each loop
 
-                interests.push(index+1);
+    var user_id = $('#container').attr('class')
 
-            } // end if loop
-        }); // end each loop
+    var updateInterests = $.post(
+      baseURL + "/users/" + user_id + "/interests",
+      {interests: interests}
+      );
 
-        var user_id = $('#container').attr('class')
-
-        var updateInterests = $.post(
-            baseURL + "/users/" + user_id + "/interests",
-            {interests: interests}
-        );
-
-        updateInterests.done(function(response){
-            $('#header').removeAttr('style');
-            $('#footer').removeAttr('style');
-            template = Handlebars.compile($("#single-event-template").html());
-            // var desc = "<p>Description: " + response.event.description + "</p>";
-            $("#container").html(template(response));
-            // $('#container a').append(desc);
+    updateInterests.done(function(response){
+      $('#header').removeAttr('style');
+      $('#footer').removeAttr('style');
+      template = Handlebars.compile($("#single-event-template").html());
+          // var desc = "<p>Description: " + response.event.description + "</p>";
+          $("#container").html(template(response));
+          // $('#container a').append(desc);
         })
 
-        updateInterests.fail(function(){
-            alert("aw man");
-        })
+    updateInterests.fail(function(){
+      alert("aw man");
+    })
 
-    }) // end submit preferences
+  }) // end submit preferences
 
+
+  $('#mypanel').on('click', '#secret', function(e){
+    e.preventDefault();
+    $('#container').html('<img id="sherif" src="img/sherif2.gif" />')
+    console.log('hello');
+  });
 }); // end document ready
